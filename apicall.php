d55524ebab04300103fd0a515c963ddda12d000c
swarnav <swarnav.das@innoraft.com>
2024-03-09 12:04:30 +0530

    FT2-43: Added comments in Advanced task 1.

--- apicall.php
+++ apicall.php
@@ -1,4 +1,10 @@
 <?php
+    /**
+     * apiCall function to call the api and get the response.
+     *
+     * @param string $url
+     * @return array
+     */
     function apiCall($url) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
